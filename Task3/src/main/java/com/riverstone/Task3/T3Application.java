8481ebf3b524a2bf15f72a905ee0855d482186c4
Prince anto <Prince anto@LAPTOP-K77I90BB>
2023-07-21 00:46:59 +0530

    Initial Commit

---
+++ Task3/src/main/java/com/riverstone/Task3/T3Application.java
@@ -0,0 +1,15 @@
+package com.riverstone.Task3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class T3Application {
+	public static void main(String[] args) {
+		SpringApplication.run(T3Application.class, args);
+		System.out.println("Successful ECOMMERCE created");
+		
+	}
+
+}
